78c2f9ebd74d3dea9e97f24b67c72c7a26a58c23
9cc9 <cyx199021@163.com>
2024-12-05 14:35:20 +0800

    Feat:edit validator

--- src/main/java/org/uestc/weglas/util/validator/RequestValidator.java
+++ src/main/java/org/uestc/weglas/util/validator/RequestValidator.java
@@ -1,6 +1,7 @@
 package org.uestc.weglas.util.validator;
 
 import org.uestc.weglas.core.enums.ResultEnum;
+import org.uestc.weglas.util.exception.AssertUtil;
 import org.uestc.weglas.util.exception.ManagerBizException;
 
 import javax.validation.ConstraintViolation;
@@ -16,12 +17,17 @@ public class RequestValidator {
 
     private static final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
 
+    /**
+     * 统一校验方法
+     *
+     * @param request
+     */
     public static void valid(Object request) {
-        if (request != null) {
-            Set<ConstraintViolation<Object>> violations = validator.validate(request);
-            if (!violations.isEmpty()) {
-                throw new ManagerBizException(ResultEnum.PARAMETER_ILLEGAL);
-            }
+        AssertUtil.notNull(request);
+
+        Set<ConstraintViolation<Object>> violations = validator.validate(request);
+        if (!violations.isEmpty()) {
+            throw new ManagerBizException(ResultEnum.PARAMETER_ILLEGAL);
         }
     }
 }
